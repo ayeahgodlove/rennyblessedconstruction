a5b12a8f7f87c17357c5850a72f2a81dc5554963
Ayeah Chanser <ayeahchanser@gmail.com>
2024-10-25 09:35:16 +0100

    update to online routes

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
   res.locals.error = req.flash("error");
-  res.locals.APP_URL = APP_URL || "rennyblessedconstruction.com";
+  res.locals.APP_URL = APP_URL || "https://www.rennyblessedconstruction.com";
   next();
 });
 
